3b42dde8c6bbc56b0b315f14204865476e7988a4
Nitesh9842 <niteshsain9842@gmail.com>
2025-06-06 20:17:09 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,12 +1,17 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify, url_for, redirect
 from com import process_single_query
 import logging
 
-app = Flask(__name__)
-# logging.basicConfig(level=logging.DEBUG)
+app = Flask(__name__, static_url_path='', static_folder='static')
+logging.basicConfig(level=logging.DEBUG)
 
 @app.route('/')
 def home():
+    return render_template('index.html')
+
+
+@app.route('/chatbot')
+def chatbot():
     return render_template('chatbot.html')
 
 @app.route('/chat', methods=['POST'])
@@ -19,7 +24,7 @@ def chat():
         app.logger.debug(f"Sending response: {response}")
         return jsonify({'response': response})
     except Exception as e:
-        app.logger.error(f"Error in chat endpoint: ")
+        app.logger.error(f"Error in chat endpoint: {e}")
         return jsonify({'response': 'An error occurred'}), 500
 
 if __name__ == '__main__':
